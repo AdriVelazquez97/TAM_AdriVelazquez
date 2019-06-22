634d5bb3205936610780727d8ce6e109d362cd9b
Adrian Velazquez <adrian.velazquez@arteia.co>
2019-06-22 20:53:00 +0200

    Created db conexion

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+module.exports = (mongoService) => {
+
+  const app = express();
+  
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(cookieParser());
+
+  app.get('/', (req, res, next) => {
+    return res.json('Dentro de la app')
+  })
+  
+  app.use('/login', (req, res, next) => {
+    return res.json('Logueado en la app')
+  })
+
+
+  return app;
+
+};
