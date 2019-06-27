403dabe10d5bf0b3be97b2fdd15dd5c1ab16dbf2
Adrian Velazquez <adrian.velazquez@arteia.co>
2019-06-27 17:11:27 +0200

    Added customers data to seed

---
+++ scripts/seed/customersData.js
@@ -0,0 +1,41 @@
+const customersData = [
+    {
+        name: 'Customer 1',
+        surname: 'Surname 1',
+        photo: 'photo 1',
+        email: 'customer1@gmail.com',
+        createdby: 'user2@gmail.com'
+    },{
+        name: 'Customer 2',
+        surname: 'Surname 2',
+        photo: 'photo 2',
+        email: 'customer2@gmail.com',
+        createdby: 'user2@gmail.com'
+    },{
+        name: 'Customer 3',
+        surname: 'Surname 3',
+        photo: 'photo 3',
+        email: 'customer3@gmail.com',
+        createdby: 'user2@gmail.com'
+    },{
+        name: 'Customer 4',
+        surname: 'Surname 4',
+        photo: 'photo 4',
+        email: 'customer4@gmail.com',
+        createdby: 'user2@gmail.com'
+    },{
+        name: 'Customer 5',
+        surname: 'Surname 5',
+        photo: 'photo 5',
+        email: 'customer5@gmail.com',
+        createdby: 'user2@gmail.com'
+    },{
+        name: 'Customer 6',
+        surname: 'Surname 6',
+        photo: 'photo 6',
+        email: 'customer6@gmail.com',
+        createdby: 'user2@gmail.com'
+    }
+]
+
+module.exports = customersData;
--- scripts/seed/seed.js
+++ scripts/seed/seed.js
@@ -2,6 +2,8 @@ const { MongoClient } = require('mongodb');
 const bcrypt = require('bcrypt')
 
 const userData = require('./userData');
+const customersData = require('./customersData')
+
 const {
     DB_NAME = 'TAM',
     MONGO_DB_URI = 'mongodb://localhost:27017',
@@ -14,15 +16,20 @@ async function seed() {
     await db.dropDatabase();
     
     const userCollection = db.collection('users')
+    const customersCollection = db.collection('customers')
 
     userData.map(value =>{
         value.password = bcrypt.hashSync(value.password, 10)
     })
 
     await userCollection.insertMany(userData)
-        .then(() => console.log("Inserted successfully"))
+        .then(() => console.log("Inserted Users successfully"))
         .catch(() => {console.log('err')})
 
+    await customersCollection.insertMany(customersData)
+        .then(() => console.log("Inserted Customers successfully"))
+        .catch(() => {console.log('err')})
+    
     process.exit(0);
 }
 
